f5b05a8a916a5ca2e44dd23817532dcf00538fd6
James Jenkins <jamesjenkinsjr@icloud.com>
2018-02-20 20:56:01 -0500

    Create basic html and form to request weather data

--- src/App.js
+++ src/App.js
@@ -5,7 +5,19 @@ class App extends Component {
   render() {
     return (
     <div>
-      
+      <h1>React Weather</h1>
+      <p>Are you blind?  Do you somehow not have the ability to look outside in your current location, or are you perhaps oddly curious about current weather for some location you are not in? GOOD NEWS -- This app gonna fix dat.</p>
+      <form>
+        <label>
+          Latitude:
+          <input type="number" placeholder="89.89"/>
+        </label>
+        <label>
+          Longitude:  
+          <input type="number" placeholder="-12.12"/>      
+        </label>
+        <button>Make it rain!</button>
+      </form>
     </div>
     );
   }
