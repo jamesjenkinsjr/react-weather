72432d08ed9f71f2d9daff9a9717f5d9e211baa0
James Jenkins <jamesjenkinsjr@icloud.com>
2018-02-22 20:44:03 -0500

    Added icons for weather, units to temp

--- client/src/CurrentWeather.js
+++ client/src/CurrentWeather.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { convertTimestamp } from './utilities.js'; 
+import Images from './images';
 const CurrentWeather = props => {
 return(
         <section>
             <ul>
                 <li>Time: {convertTimestamp(props.time)}</li>
                 <li>Summary: {props.summary}</li>
-                <li>Icon: {props.icon}</li>
-                <li>Temperature: {props.temperature}</li>
+                <li>Icon: <img src={Images(props.icon)} alt={props.icon}/></li>
+                <li>Temperature: {props.temperature} &deg;F</li>
             </ul>
         </section>
     );
---
+++ client/src/images/index.js
@@ -0,0 +1,10 @@
+import rain from './Emoticon-Rain.gif';
+
+const selection = (name) => {
+    if(name === 'rain'){
+        return rain;
+    }
+    return rain;
+}
+
+export default selection;
